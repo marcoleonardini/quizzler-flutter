c67ceb7ad5dbd45756e25a706092ba50b3b401f9
Marco Leonardini <marco_leonardini@hotmail.com>
2020-02-28 19:55:07 -0400

    Added Quizz Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/quiz-brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +27,18 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [];
+
+  String text = '';
+  bool answer = false;
+  final QuizBrain quizBrain = QuizBrain();
+  @override
+  void initState() {
+    text = quizBrain.getQuestionText();
+    answer = quizBrain.getCorrectAnswer();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +51,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                text,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +75,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                _updateQuestionIndex(true, context);
               },
             ),
           ),
@@ -79,19 +93,50 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                _updateQuestionIndex(false, context);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
-}
 
+  void _updateQuestionIndex(bool _answer, BuildContext context) {
+    if (_answer == answer) {
+      scoreKeeper.add(Icon(
+        Icons.check,
+        color: Colors.green,
+      ));
+    } else {
+      scoreKeeper.add(Icon(
+        Icons.close,
+        color: Colors.red,
+      ));
+    }
+
+    if (quizBrain.isFinished()) {
+      Alert(
+              context: context,
+              title: "Finished",
+              desc: "You have reach the end of the Quizz.")
+          .show();
+      quizBrain.reset();
+      scoreKeeper = [];
+    } else {
+      quizBrain.nextQuestion();
+    }
+
+    text = quizBrain.getQuestionText();
+    answer = quizBrain.getCorrectAnswer();
+
+    setState(() {});
+  }
+}
 /*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
+ false,
+ true,
+ true,*/
---
+++ lib/quiz-brain.dart
@@ -0,0 +1,77 @@
+import 'question.dart';
+
+class QuizBrain {
+  int _questionNumber = 0;
+
+  List<Question> _questionBank = [
+    Question(
+        question: 'Some cats are actually allergic to humans', answer: true),
+    Question(
+        question: 'You can lead a cow down stairs but not up stairs.',
+        answer: false),
+    Question(
+        question: 'Approximately one quarter of human bones are in the feet.',
+        answer: true),
+    Question(question: 'A slug\'s blood is green.', answer: true),
+    Question(
+        question: 'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".',
+        answer: true),
+    Question(
+        question: 'It is illegal to pee in the Ocean in Portugal.',
+        answer: true),
+    Question(
+        question:
+            'No piece of square dry paper can be folded in half more than 7 times.',
+        answer: false),
+    Question(
+        question:
+            'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        answer: true),
+    Question(
+        question:
+            'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        answer: false),
+    Question(
+        question:
+            'The total surface area of two human lungs is approximately 70 square metres.',
+        answer: true),
+    Question(
+        question: 'Google was originally called \"Backrub\".', answer: true),
+    Question(
+        question:
+            'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        answer: true),
+    Question(
+        question:
+            'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        answer: true),
+  ];
+
+  void nextQuestion() {
+    if (_questionNumber < _questionBank.length - 1) {
+      _questionNumber++;
+    }
+  }
+
+  String getQuestionText() {
+    return _questionBank[_questionNumber].question;
+  }
+
+  bool getCorrectAnswer() {
+    return _questionBank[_questionNumber].answer;
+  }
+
+  bool isFinished() {
+    return _questionBank.length - 1 == _questionNumber;
+  }
+
+  void reset() {
+    _questionNumber = 0;
+  }
+
+  //TODO: Step 3 Part A - Create a method called isFinished() here that checks to see if we have reached the last question. It should return (have an output) true if we've reached the last question and it should return false if we're not there yet.
+
+  //TODO: Step 3 Part B - Use a print statement to check that isFinished is returning true when you are indeed at the end of the quiz and when a restart should happen.
+
+  //TODO: Step 4 Part B - Create a reset() method here that sets the questionNumber back to 0.
+}
